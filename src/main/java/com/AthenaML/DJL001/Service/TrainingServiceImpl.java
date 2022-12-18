d08e080bf0ad695ae5d6add0c8166031ef4d7628
waynesidney <wayne.oguda.sidney@gmail.com>
2022-12-18 13:37:32 +0300

    cleared errors in the training process

--- src/main/java/com/AthenaML/DJL001/Service/TrainingServiceImpl.java
+++ src/main/java/com/AthenaML/DJL001/Service/TrainingServiceImpl.java
@@ -3,6 +3,7 @@ package com.AthenaML.DJL001.Service;
 import ai.djl.Model;
 import ai.djl.basicdataset.cv.classification.Mnist;
 import ai.djl.metric.Metrics;
+import ai.djl.ndarray.NDList;
 import ai.djl.ndarray.types.Shape;
 import ai.djl.nn.Activation;
 import ai.djl.training.EasyTrain;
@@ -10,6 +11,7 @@ import ai.djl.training.Trainer;
 import ai.djl.training.TrainingResult;
 import ai.djl.training.dataset.Dataset;
 import ai.djl.training.dataset.RandomAccessDataset;
+import ai.djl.translate.TranslateException;
 import com.AthenaML.DJL001.Helper.HelperService;
 import com.AthenaML.DJL001.MultiLayerPerceptron;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -27,6 +29,9 @@ import java.nio.file.Paths;
 @Service
 public class TrainingServiceImpl implements TrainingService{
 
+    private static final String MODEL_NAME = "model_mnistClassification";
+
+    private static final String MODEL_DIRE = "~/AthenaMLJava/DJL-001/src/main/java/com/AthenaML/DJL001/Djl001Application.java";
     private static final int BATCH_SIZE = 32;
     private static final int LIMIT = 40;
     private static final int EPOCH = 25;
@@ -34,10 +39,10 @@ public class TrainingServiceImpl implements TrainingService{
     private HelperService helperService;
     @Override
     public TrainingResult trainMnistDataSet() throws IOException {
-        //add a MODEL_NAME in the newInstance() method as parameter
-        try(Model model = Model.newInstance() {
+        try(Model model = Model.newInstance(MODEL_NAME)){
+
             model.setBlock(new MultiLayerPerceptron
-                    (Mnist.IMAGE_HEIGHT * Mnist.IMAGE_WIDTH, Mnist.NUM_CLASSES, new int[]{128, 64}, Activation::sigmoid));
+                    (Mnist.IMAGE_HEIGHT * Mnist.IMAGE_WIDTH, Mnist.NUM_CLASSES, new int[]{128, 64}, Activation.));
             RandomAccessDataset trainingSet = helperService.getDataSet(Dataset.Usage.TRAIN, BATCH_SIZE, LIMIT);
             RandomAccessDataset validateSet = this.helperService.getDataSet(Dataset.Usage.TEST, BATCH_SIZE, LIMIT);
 
@@ -50,6 +55,8 @@ public class TrainingServiceImpl implements TrainingService{
                 model.setProperty("Epoch", String.valueOf(EPOCH));
                 model.save(modelDire, MODEL_NAME);
                 return trainer.getTrainingResult();
+            } catch (TranslateException e) {
+                throw new RuntimeException(e);
             }
         }
     }
